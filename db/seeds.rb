21d22c6a888d59802b0c87bc862e72396ab66a93
Andrew Talle <andrew.talle@ibm.com>
2019-07-26 16:47:11 -0700

    github

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Course.create(short_name: "Fantasy204", name: "How to speak Elvish", description: "Come and learn how to speak like Orlando Bloom and you too can play Legolas!")
+Course.create(short_name: "Fantasy202", name: "How to use a Lightsaber", description: "In this course Darth Vader will prepare students on how to slay any Jedi they might encounter across the galaxy.")
+Course.create(short_name: "Social101", name: "Intro to Instagram", description: "So you don't have to get a real career, instead learn how to become Insta-famous and live a career of self-indulgence")
+Course.create(short_name: "Coffee201", name: "Artisan Coffee", description: "Espressos, Pour-overs, Cold-Brews, Ohh-My!!!")
+Course.create(short_name: "Music111", name: "Joy of Guitar", description: "Come learn the worlds favorite instrument")
